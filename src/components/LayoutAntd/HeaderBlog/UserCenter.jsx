f1dbc7f4c86977d9e7ae8b472e95867192bca954
XiaoFengVIVY <2352671485@qq.com>
2022-04-23 18:57:00 +0800

    个人中心组件更新： 将class组件改成函数组件
    更改原因：用户登陆后组件不能立马刷新，需手动刷新一次

--- src/components/LayoutAntd/HeaderBlog/UserCenter.jsx
+++ src/components/LayoutAntd/HeaderBlog/UserCenter.jsx
@@ -1,32 +1,24 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 // import { Link } from "react-router-dom";
 import { BASE_URL } from "../../../utils/url";
 import { NavLink } from "react-router-dom";
 import { Avatar, Button, Popover } from "antd";
 import axios from "axios";
 
-class UserCenter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: 0,
-            isLoggedIn: false,
-            username: "未登录",
-            useravatar: ""
-        };
-        this.logout = this.logout.bind(this);
-    }
+function UserCenter() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [id, setId] = useState("");
+    const [username, setUsername] = useState("未登录");
+    const [useravatar, setUseravatar] = useState("");
 
-    componentDidMount() {
+    useEffect(() => {
         if (localStorage.username === null) {
             console.log("usercenter2: ", localStorage.username);
         } else {
             axios
                 .post(
                     BASE_URL + "/users/loginStatus",
-                    {
-                        username: localStorage.username,
-                    },
+                    {},
                     {
                         headers: {
                             authorization:
@@ -37,84 +29,70 @@ class UserCenter extends Component {
                 )
                 .then((res) => {
                     if (res.data.code === 1) {
-                        this.setState({
-                            isLoggedIn: true,
-                            id: res.data.data.id,
-                            username: res.data.data.username,
-                            useravatar: res.data.data.useravatar,
-                        });
+                        setIsLoggedIn(true);
+                        setId(res.data.data.id);
+                        setUsername(res.data.data.username);
+                        setUseravatar(res.data.data.useravatar);
                     } else {
-                        this.setState({
-                            isLoggedIn: false,
-                        });
+                        setIsLoggedIn(false);
                     }
                 });
         }
-    }
+    });
 
-    logout() {
-        this.setState({
-            isLoggedIn: false,
-        });
+    const logout = () => {
+        setIsLoggedIn(false);
         localStorage.removeItem("token");
-    }
+    };
 
-    render() {
-        const isLoggedIn = this.state.isLoggedIn;
-        const content = (
+    const content = (
+        <div>
+            <Button type="text">
+                <NavLink to={`/UserCenter/${id}`}>个人中心</NavLink>
+            </Button>
             <div>
-                <Button type="text">
-                    <NavLink to={`/UserCenter/${this.state.id}`}>
-                        个人中心
-                    </NavLink>
+                <Button type="link" href="/UserCenter">
+                    我的收藏
                 </Button>
-                <div>
-                    <Button type="link" href="/UserCenter">
-                        我的收藏
-                    </Button>
-                </div>
-                <div>
-                    <Button onClick={this.logout} type="text">
-                        退出登录
-                    </Button>
-                </div>
             </div>
-        );
+            <div>
+                <Button onClick={logout} type="text">
+                    退出登录
+                </Button>
+            </div>
+        </div>
+    );
 
-        return (
-            <div className="userCenter">
-                {isLoggedIn ? (
-                    <section>
-                        <Popover
-                            placement="bottom"
-                            content={content}
-                            title="个人中心"
-                        >
-                            <Avatar
-                                size={36}
-                                src={BASE_URL + "/" + this.state.useravatar}
-                            />
-                            <Button type="link">{this.state.username}</Button>
-                        </Popover>
-                        <Button
-                            className="btn"
-                            type="link"
-                            style={{ lineHeight: "64px" }}
-                            href="/Write"
-                        >
-                            写文章
-                        </Button>
-                    </section>
-                ) : (
-                    <section>
-                        <Button className="btn" type="button" href="/Login">
-                            登录注册
-                        </Button>
-                    </section>
-                )}
-            </div>
-        );
-    }
+    return (
+        <div className="userCenter">
+            {isLoggedIn ? (
+                <section>
+                    <Popover
+                        placement="bottom"
+                        content={content}
+                        title="个人中心"
+                    >
+                        <Avatar size={36} src={BASE_URL + "/" + useravatar} />
+                        <Button type="link">{username}</Button>
+                    </Popover>
+                    <Button
+                        className="btn"
+                        type="link"
+                        style={{ lineHeight: "64px" }}
+                        href="/Write"
+                    >
+                        写文章
+                    </Button>
+                </section>
+            ) : (
+                <section>
+                    <Button className="btn" type="button" href="/Login">
+                        登录注册
+                    </Button>
+                </section>
+            )}
+        </div>
+    );
 }
 
 export default UserCenter;
